46f83db02e925fa413a9253d4a1e5cf528c747e2
villysiu <villysiu@gmail.com>
2021-06-08 11:40:44 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+
+function findMatching(drivers, driverString) {
+    let matched = drivers.filter(n => {
+      return n.match(new RegExp(driverString, 'i'));
+    });
+    return matched;
+  }
+  function fuzzyMatch(drivers, driverString) {
+    let matched = drivers.filter(n => {
+      return n.match(new RegExp('^' + driverString));
+    });
+    return matched;
+  }
+  
+  function matchName(drivers, driverString) {
+    let matched = drivers.filter(n => {
+      return n.name.match(new RegExp(driverString, 'i'));
+    });
+    return matched;
+  }
